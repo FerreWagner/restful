b6c8b1831dd1ba4e0de9e5d6940f8901fd3f43f2
FerreWagner <18408229270@163.com>
2018-01-09 00:17:57 +0800

    not fixed bug

--- application/admin/controller/Image.php
+++ application/admin/controller/Image.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\admin\controller;
 
+use app\common\lib\Upload;
 /**
  * 后台图片上传逻辑
  * Class Image
@@ -8,10 +9,31 @@ namespace app\admin\controller;
  */
 class Image extends Base
 {
-    public function upload()
+    public function upload0()
     {
 
         return 1;
+    }
+
+    /**
+     * 七牛图片上传
+     */
+    public function upload() {
+        try {
+            $image = Upload::image();
+        }catch (\Exception $e) {
+            echo json_encode(['status' => 0, 'message' => $e->getMessage()]);
+        }
+        if($image) {
+            $data = [
+                'status' => 1,
+                'message' => 'OK',
+                'data' => config('qiniu.image_url').'/'.$image,
+            ];
+            echo json_encode($data);exit;
+        }else {
+            echo json_encode(['status' => 0, 'message' => '上传失败']);
+        }
     }
 
 }
---
+++ application/extra/qiniu.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 15736
+ * Date: 2018/1/8
+ * Time: 23:15
+ */
+
+return [
+    'ak' => 'xxx',
+    'sk' => 'sss',
+    'bucket' => 'ferre',
+    'image_url' => 'http://p28s35qla.bkt.clouddn.com',
+];
